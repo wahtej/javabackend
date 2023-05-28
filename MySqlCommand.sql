d7cb030dd11bff52c1cc21cf581ca42be7c5384f
wahtej <125274697+wahtej@users.noreply.github.com>
2023-05-28 12:55:57 +0530

    Update MySqlCommand.sql

--- MySqlCommand.sql
+++ MySqlCommand.sql
@@ -1,5 +1,6 @@
-For Creating table in database:
-CREATE TABLE `admin` (
+
+-- For Creating table in database:
+CREATE TABLE IF NOT EXISTS `admin` (
   `admin_id` int(11) NOT NULL,
   `email_id` varchar(255) DEFAULT NULL,
   `first_name` varchar(255) DEFAULT NULL,
@@ -10,17 +11,17 @@ CREATE TABLE `admin` (
   UNIQUE KEY `UK_72auwcxdo976d396pv0f1xc1w` (`email_id`)
 ) ;
 
-For to insert data into admin database:
+-- For to insert data into admin database:
 
-INSERT INTO `online-hosp-mngmt`.`admin`
+INSERT INTO `admin`
 (`admin_id`,`email_id`,`first_name`,`last_name`,`password`,`phone_no`)
 VALUES(<{admin_id: }>,<{email_id: }>,<{first_name: }>,<{last_name: }>,<{password: }>,<{phone_no: }>);
-
+/*
 admin_id:must be number
-email_id; email should contain ‘@’ symbol. 
+email_id; email should contain â€˜@â€™ symbol. 
 first_name:first name must be String 
 Last_name: last name must be in String
 password: password should be in bcrypt form.
 $2a$10$tBqnc4waIPGy5meW0ICSG.Nz0Jpi3ecq.oxBRZmknqtgWMf9bMPJW (Motivity@123);
 phone_no: phone number must be contain 10 number.
-
+*/
